6a57144210d0e511bea063a563355f00e8e025f9
Otong Sunandar <ostrue@gmail.com>
2020-08-30 05:50:43 +0700

    add simple data

--- app/src/main/java/com/klapertart/myapplication/MainActivity.kt
+++ app/src/main/java/com/klapertart/myapplication/MainActivity.kt
@@ -2,11 +2,25 @@ package com.klapertart.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import com.klapertart.myapplication.data.User
+import com.klapertart.myapplication.db.AppDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "dbsample"
+        ).allowMainThreadQueries().build()
+
+
+        val user = User(1,"Abdillah","Hamka")
+        db.userDao().insertAll(user)
+
+        println(db.userDao().getAll())
     }
 }
